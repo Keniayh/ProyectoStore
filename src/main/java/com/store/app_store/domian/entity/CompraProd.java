09b22cbc25e5d67530b83ed13a8ddc730db872e8
Kenia <keniayhdiaz@gmail.com>
2024-09-27 12:02:57 -0500

    feat: :sparkles: Add new features

--- src/main/java/com/store/app_store/domian/entity/CompraProd.java
+++ src/main/java/com/store/app_store/domian/entity/CompraProd.java
@@ -1,5 +1,10 @@
 package com.store.app_store.domian.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
 public class CompraProd {
 
 }
--- src/main/java/com/store/app_store/domian/entity/Producto.java
+++ src/main/java/com/store/app_store/domian/entity/Producto.java
@@ -1,5 +1,10 @@
 package com.store.app_store.domian.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
 
 }
